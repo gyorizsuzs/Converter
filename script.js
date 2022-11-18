27ce38f7dccb84a1195c8c61b4c8e9afc631f448
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-11-18 11:53:41 +0100

    creating converter function - working with minus and zero

--- script.js
+++ script.js
@@ -101,6 +101,37 @@ function numToStr(numeral) {
   /*               Base Logic                */
 
   /*******************************************/
+
+  function doTheMath(number, orderValueIndex = -1) {
+    const result = [];
+
+    if (number === 0) return 'zero';
+
+    // Numbers below 0
+
+    if (number < 0) {
+      result.push('minus');
+      result.push(doTheMath(-number, orderValueIndex));
+      return result.join(' ');
+    }
+  }
+  return doTheMath(number);
+}
+
+function converter(inputData) {
+  const output = document.getElementById('output');
+
+  try {
+    output.innerText = numToStr(inputData);
+    console.log(inputData, numToStr(inputData));
+  } catch (err) {
+    output.innerText = err.message;
+  }
+}
+
+function handleClick() {
+  const inputData = document.getElementById('input').value;
+  converter(inputData);
 }
 
 /*******************************************/
